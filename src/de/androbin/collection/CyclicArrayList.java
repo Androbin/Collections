4493cd4ee3e15d3860318cb40ba16fc56c1bfc7d
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-01-20 15:38:16 +0100

    Extract CyclicArrayList

---
+++ src/de/androbin/collection/CyclicArrayList.java
@@ -0,0 +1,112 @@
+package de.androbin.collection;
+
+import static de.androbin.collection.CyclicArrayUtil.*;
+import static de.androbin.math.util.ints.IntMathUtil.*;
+import java.util.*;
+
+public final class CyclicArrayList<E> extends AbstractList<E> {
+  private Object[] array;
+  private int count;
+  private int pointer;
+  
+  public CyclicArrayList() {
+    this( 100 );
+  }
+  
+  public CyclicArrayList( final int initialCapacity ) {
+    this.array = new Object[ initialCapacity ];
+  }
+  
+  @ Override
+  public void add( final int index, final E element ) {
+    if ( array.length <= count ) {
+      this.array = toArray( count << 1 );
+      this.pointer = 0;
+    }
+    
+    if ( index << 1 < count ) {
+      cyclicarraycopy( array, pointer, shiftUp( pointer - 1, array.length ), index + 1 );
+      pointer = shiftUp( pointer - 1, array.length );
+    } else {
+      cyclicarraycopy( array, indexFor( index ),
+          shiftDown( pointer + index + 1, array.length ), count - index );
+    }
+    
+    count++;
+    set( index, element );
+  }
+  
+  @ Override
+  public void clear() {
+    count = 0;
+  }
+  
+  @ Override
+  public E get( final int index ) {
+    @ SuppressWarnings( "unchecked" )
+    final E element = (E) array[ indexFor( index ) ];
+    return element;
+  }
+  
+  private int indexFor( final int index ) {
+    return shiftDown( pointer + index, array.length );
+  }
+  
+  public void intoArray( final Object[] target ) {
+    if ( pointer + count <= array.length ) {
+      System.arraycopy( array, pointer, target, 0, count );
+    } else {
+      System.arraycopy( array, pointer, target, 0, array.length - pointer );
+      System.arraycopy( array, 0, target, array.length - pointer, pointer + count - array.length );
+    }
+  }
+  
+  public void move( final int src, final int dest ) {
+    if ( src == dest ) {
+      return;
+    }
+    
+    final E element = get( src );
+    
+    if ( src < dest ) {
+      cyclicarraycopy( array, indexFor( src + 1 ), indexFor( src ), dest - src );
+    } else {
+      cyclicarraycopy( array, indexFor( dest ), indexFor( dest + 1 ), src - dest );
+    }
+    
+    set( dest, element );
+  }
+  
+  @ Override
+  public E remove( final int index ) {
+    final E element = get( index );
+    
+    if ( index << 1 < count ) {
+      cyclicarraycopy( array, pointer, indexFor( 1 ), index );
+      pointer = indexFor( 1 );
+    } else {
+      cyclicarraycopy( array, indexFor( index + 1 ), indexFor( index ), count - index - 1 );
+    }
+    
+    count--;
+    return element;
+  }
+  
+  @ Override
+  public E set( final int index, final E element ) {
+    final E previous = get( index );
+    array[ indexFor( index ) ] = element;
+    return previous;
+  }
+  
+  @ Override
+  public int size() {
+    return count;
+  }
+  
+  public Object[] toArray( final int capacity ) {
+    final Object[] a = new Object[ capacity ];
+    intoArray( a );
+    return a;
+  }
+}
--- src/de/androbin/collection/PriorityQueue.java
+++ src/de/androbin/collection/PriorityQueue.java
@@ -1,62 +1,48 @@
 package de.androbin.collection;
 
-import static de.androbin.collection.CyclicArrayUtil.*;
-import static de.androbin.math.util.ints.IntMathUtil.*;
 import java.util.*;
-import java.util.function.*;
 
-public final class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
-  private Object[] nodes;
-  private int count;
-  private int pointer;
+public final class PriorityQueue<E extends Comparable<E>> extends AbstractQueue<E> {
+  private CyclicArrayList<E> nodes;
   
   public PriorityQueue() {
     this( 100 );
   }
   
   public PriorityQueue( final int initialCapacity ) {
-    this.nodes = new Object[ initialCapacity ];
+    this.nodes = new CyclicArrayList<>( initialCapacity );
   }
   
-  public E extract( final int index ) {
-    final E node = getNode( index );
-    
-    if ( index << 1 < count ) {
-      cyclicarraycopy( nodes, pointer, shiftDown( pointer + 1, nodes.length ), index );
-      pointer = shiftDown( pointer + 1, nodes.length );
-    } else {
-      cyclicarraycopy( nodes, shiftDown( pointer + index + 1, nodes.length ),
-          shiftDown( pointer + index, nodes.length ), count - index - 1 );
-    }
-    
-    count--;
-    return node;
+  @ Override
+  public void clear() {
+    nodes.clear();
   }
   
-  private E getNode( final int index ) {
+  @ Override
+  public boolean contains( final Object o ) {
     @ SuppressWarnings( "unchecked" )
-    final E node = (E) nodes[ shiftDown( pointer + index, nodes.length ) ];
-    return node;
+    final int index = indexOf( (E) o );
+    return index != -1;
   }
   
-  public int indexOf( final E e ) {
-    final int m = indexOfPriority( e );
+  public int indexOf( final E element ) {
+    final int m = indexOfPriority( element );
     
     if ( m != -1 ) {
       E node;
       
       for ( int i = m; i >= 0; i-- ) {
-        if ( ( node = getNode( i ) ) == e ) {
+        if ( ( node = nodes.get( i ) ) == element ) {
           return i;
-        } else if ( e.compareTo( node ) != 0 ) {
+        } else if ( element.compareTo( node ) != 0 ) {
           break;
         }
       }
       
-      for ( int i = m + 1; i < count; i++ ) {
-        if ( ( node = getNode( i ) ) == e ) {
+      for ( int i = m + 1; i < nodes.size(); i++ ) {
+        if ( ( node = nodes.get( i ) ) == element ) {
           return i;
-        } else if ( e.compareTo( node ) != 0 ) {
+        } else if ( element.compareTo( node ) != 0 ) {
           break;
         }
       }
@@ -65,13 +51,17 @@ public final class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
     return -1;
   }
   
-  public int indexOfInsertion( final E e ) {
+  public int indexOfInsertion( final E element ) {
+    if ( isEmpty() ) {
+      return 0;
+    }
+    
     int l = 0;
-    int r = count - 1;
+    int r = nodes.size() - 1;
     
     while ( true ) {
       final int m = ( l + r ) / 2;
-      final int c = e.compareTo( getNode( m ) );
+      final int c = element.compareTo( nodes.get( m ) );
       
       if ( c == 0 ) {
         return m + 1;
@@ -85,20 +75,20 @@ public final class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
     }
   }
   
-  public int indexOfPriority( final E e ) {
-    if ( count == 0 ) {
+  public int indexOfPriority( final E element ) {
+    if ( isEmpty() ) {
       return -1;
     }
     
     int l = 0;
-    int r = count - 1;
+    int r = nodes.size() - 1;
     
     int m;
     int c;
     
     while ( true ) {
       m = ( l + r ) / 2;
-      c = e.compareTo( getNode( m ) );
+      c = element.compareTo( nodes.get( m ) );
       
       if ( c == 0 ) {
         return m;
@@ -112,214 +102,40 @@ public final class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
     }
   }
   
-  public void insert( final int index, final E e ) {
-    if ( nodes.length <= count ) {
-      this.nodes = toArray( count << 1 );
-      this.pointer = 0;
-    }
-    
-    if ( index << 1 < count ) {
-      cyclicarraycopy( nodes, pointer, shiftUp( pointer - 1, nodes.length ), index + 1 );
-      pointer = shiftUp( pointer - 1, nodes.length );
-    } else {
-      cyclicarraycopy( nodes, shiftDown( pointer + index, nodes.length ),
-          shiftDown( pointer + index + 1, nodes.length ), count - index );
-    }
-    
-    count++;
-    setNode( index, e );
-  }
-  
-  public void intoArray( final Object[] array ) {
-    if ( pointer + count <= nodes.length ) {
-      System.arraycopy( nodes, pointer, array, 0, count );
-    } else {
-      System.arraycopy( nodes, pointer, array, 0, nodes.length - pointer );
-      System.arraycopy( nodes, 0, array, nodes.length - pointer, pointer + count - nodes.length );
-    }
-  }
-  
-  public void reorder( final E e ) {
-    reorder( indexOf( e ) );
-  }
-  
-  public void reorder( final int index ) {
-    final E node = getNode( index );
-    final int insertIndex = indexOfInsertion( node );
-    
-    if ( index <= insertIndex ) {
-      cyclicarraycopy( nodes, shiftDown( pointer + index + 1, nodes.length ),
-          shiftDown( pointer + index, nodes.length ), insertIndex - index - 1 );
-    } else {
-      cyclicarraycopy( nodes, shiftDown( pointer + insertIndex + 1, nodes.length ),
-          shiftDown( pointer + insertIndex + 2, nodes.length ), index - insertIndex - 1 );
-    }
-    
-    setNode( insertIndex, node );
-  }
-  
-  private void setNode( final int index, final E e ) {
-    nodes[ shiftDown( pointer + index, nodes.length ) ] = e;
-  }
-  
-  public Object[] toArray( final int capacity ) {
-    final Object[] a = new Object[ capacity ];
-    intoArray( a );
-    return a;
-  }
-  
-  @ Override
-  public int size() {
-    return count;
-  }
-  
-  @ Override
-  public boolean isEmpty() {
-    return count == 0;
-  }
-  
-  @ Override
-  public boolean contains( final Object o ) {
-    @ SuppressWarnings( "unchecked" )
-    final int index = indexOf( (E) o );
-    return index != -1;
-  }
-  
   @ Override
   public Iterator<E> iterator() {
-    return new Iterator<E>() {
-      private int index;
-      
-      @ Override
-      public void forEachRemaining( final Consumer< ? super E> action ) {
-        while ( hasNext() ) {
-          action.accept( next() );
-        }
-      }
-      
-      @ Override
-      public boolean hasNext() {
-        return index < count;
-      }
-      
-      @ Override
-      public E next() {
-        return getNode( index++ );
-      }
-      
-      @ Override
-      public void remove() {
-        extract( index - 1 );
-      }
-    };
+    return nodes.iterator();
   }
   
   @ Override
-  public Object[] toArray() {
-    return toArray( nodes.length );
-  }
-  
-  @ Override
-  public <T> T[] toArray( final T[] a ) {
-    final T[] b = a.length >= nodes.length ? a : Arrays.copyOf( a, nodes.length );
-    intoArray( b );
-    return b;
-  }
-  
-  @ Override
-  public boolean remove( final Object o ) {
-    @ SuppressWarnings( "unchecked" )
-    final int index = indexOf( (E) o );
-    
-    if ( index == -1 ) {
-      return false;
-    } else {
-      extract( index );
-      return true;
-    }
-  }
-  
-  @ Override
-  public boolean containsAll( final Collection< ? > c ) {
-    return c.parallelStream().allMatch( this::contains );
-  }
-  
-  @ Override
-  public boolean addAll( final Collection< ? extends E> c ) {
-    // TODO prevent short circuting
-    return c.parallelStream().map( this::add ).reduce( ( a, b ) -> a || b ).get();
-  }
-  
-  @ Override
-  public boolean removeAll( final Collection< ? > c ) {
-    // TODO prevent short circuting
-    return c.parallelStream().map( this::remove ).reduce( ( a, b ) -> a || b ).get();
-  }
-  
-  @ Override
-  public boolean retainAll( final Collection< ? > c ) {
-    final Iterator<E> iter = iterator();
-    
-    boolean changed = false;
-    
-    while ( iter.hasNext() ) {
-      if ( !c.contains( iter.next() ) ) {
-        iter.remove();
-        changed = true;
-      }
-    }
-    
-    return changed;
-  }
-  
-  @ Override
-  public void clear() {
-    Arrays.fill( this.nodes, null );
-    this.pointer = 0;
-    this.count = 0;
-  }
-  
-  @ Override
-  public boolean add( final E e ) {
-    return offer( e );
-  }
-  
-  @ Override
-  public boolean offer( final E e ) {
-    if ( count == 0 ) {
-      setNode( 0, e );
-      count++;
-    } else {
-      insert( indexOfInsertion( e ), e );
-    }
-    
+  public boolean offer( final E element ) {
+    nodes.add( indexOfInsertion( element ), element );
     return true;
   }
   
   @ Override
-  public E remove() {
-    return poll();
+  public E peek() {
+    return nodes.get( 0 );
   }
   
   @ Override
   public E poll() {
-    @ SuppressWarnings( "unchecked" )
-    final E node = (E) nodes[ pointer ];
+    return nodes.remove( 0 );
+  }
+  
+  public void reorder( final int index ) {
+    final E node = nodes.get( index );
+    final int insertIndex = indexOfInsertion( node );
     
-    pointer = shiftDown( pointer + 1, nodes.length );
-    count--;
-    return node;
+    nodes.move( index, insertIndex );
+  }
+  
+  public void reorder( final E element ) {
+    reorder( indexOf( element ) );
   }
   
   @ Override
-  public E element() {
-    return peek();
-  }
-  
-  @ Override
-  public E peek() {
-    @ SuppressWarnings( "unchecked" )
-    final E node = (E) nodes[ pointer ];
-    return node;
+  public int size() {
+    return nodes.size();
   }
 }
